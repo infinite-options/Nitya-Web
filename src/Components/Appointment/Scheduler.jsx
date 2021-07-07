a03d2cdd93bc6065f812c470685875e198f6c48d
Ashley Ma <euyam458689881@gmail.com>
2021-07-06 17:02:24 -0700

    fix endpoint, change style

--- src/Components/Appointment/Scheduler.jsx
+++ src/Components/Appointment/Scheduler.jsx
@@ -10,6 +10,14 @@ import { useElements, useStripe, CardElement } from "@stripe/react-stripe-js";
 import { makeStyles } from "@material-ui/core/styles";
 import { MyContext } from "../../App";
 import moment from "moment";
+import {
+  Card,
+  Button,
+  CardImg,
+  CardTitle,
+  CardText,
+  CardBody,
+} from "reactstrap";
 
 export default function Scheduler(props) {
   const elements = useElements();
@@ -27,6 +35,8 @@ export default function Scheduler(props) {
       serviceArr.forEach((element) => {
         if (element.treatment_uid === treatment_uid) {
           setElementToBeRendered(element);
+          console.log("duration" + elementToBeRendered.duration);
+          setDuration(elementToBeRendered.duration);
         }
       });
     }
@@ -40,7 +50,8 @@ export default function Scheduler(props) {
 
   const [apiDateString, setApiDateString] = useState(null);
   const [timeSlots, setTimeSlots] = useState([]);
-  const [apptHourDurition, setApptHourDurition] = useState(false);
+
+  const [duration, setDuration] = useState(null);
 
   //For Axios.Post
   const [purchaseDate, setPurchaseDate] = useState(new Date());
@@ -57,6 +68,7 @@ export default function Scheduler(props) {
   // for hide and show
   const [timeSelected, setTimeSelected] = useState(false);
   const [submitted, setSubmitted] = useState(false);
+  const [bookNowClicked, setBookNowClicked] = useState(false);
 
   //A lot of event handlers
 
@@ -158,19 +170,19 @@ export default function Scheduler(props) {
     if (dateHasBeenChanged) {
       axios
         .get(
-          "https://mfrbehiqnb.execute-api.us-west-1.amazonaws.com/dev/api/v2/calendar/" +
-            // treatment_uid +
-            // "/" +
-            apiDateString
+          "https://mfrbehiqnb.execute-api.us-west-1.amazonaws.com/dev/api/v2/availableAppointments/" +
+            apiDateString +
+            "/" +
+            duration
         )
         .then((res) => {
           // console.log(res.data.result.available_timeslots);
           // setTimeSlots(res.data.result.available_timeslots);
           console.log(res);
           console.log("This is the information we got" + res.data);
-          console.log(res.data[0]);
-          console.log(res.data[0].appt_start);
-          setTimeSlots(res.data);
+          console.log(res);
+          console.log(res);
+          setTimeSlots(res.data.result);
           //setTimeSlot(res.data); // for one hour appt
           console.log("Timeslots Array " + timeSlots);
         });
@@ -178,36 +190,14 @@ export default function Scheduler(props) {
     setDateHasBeenChanged(false);
   });
 
-  function setTimeslot(appt) {
-    if (apptHourDurition) {
-      const oneHourAppt = [];
-      for (let i = 0; i < appt.length - 1; i++) {
-        var date1 = new moment(apiDateString + " " + appt[i].appt_start);
-        var date2 = new moment(apiDateString + " " + appt[i + 1].appt_start);
-        if (date1.add(30, "minutes").format() == date2.format()) {
-          oneHourAppt.push(appt[i]);
-        }
-      }
-      setTimeSlots(oneHourAppt);
-    }
-  }
-
-  // function renderAvailableAppts() {
-  //   return timeSlots.map((element) => (
-  //     <Button onClick={() => selectApptTime(element)}>
-  //       {element.appt_start}
-  //     </Button>
-  //   ));
-  // }
-
   function renderAvailableApptsVertical() {
     return timeSlots.map((element) => (
       <Row>
         <button
           className={classes.button}
-          onClick={() => selectApptTime(element.appt_start)}
+          onClick={() => selectApptTime(element.begin_time)}
         >
-          {element.appt_start}
+          {element.begin_time}
         </button>{" "}
       </Row>
     ));
@@ -216,6 +206,14 @@ export default function Scheduler(props) {
   function selectApptTime(element) {
     setSelectedTime(element);
     setTimeSelected(true);
+  }
+
+  function clickBookNow() {
+    console.log("button clicked");
+    if (!bookNowClicked) {
+      setBookNowClicked(true);
+      console.log("clicked is set to true");
+    }
   }
 
   function sendToDatabase() {
@@ -335,18 +333,16 @@ export default function Scheduler(props) {
 
   const useStyles = makeStyles({
     h1: {
-      fontSize: "42px",
+      fontSize: "24px",
       // padding: "20px 0",
       color: "#B28D42",
       fontFamily: "Hoefler",
     },
     content: {
-      fontSize: "32px",
+      fontSize: "22px",
       fontFamily: "SFProDisplayRegular",
-      // wordWrap: "break-word",
       color: "#B28D42",
-      // lineHeight: "1.4",
-      textAlign: "left",
+      textAlign: "center",
     },
     selectTime: {
       fontSize: "32px",
@@ -357,13 +353,13 @@ export default function Scheduler(props) {
     },
     container: {
       margin: "50px auto",
-      width: "80%",
-      padding: "50px 20px",
+      width: "980px", //"80%",
+      padding: "50px 50px",
       backgroundColor: "white",
     },
     CalendarContainer: {
       margin: "auto",
-      width: "80%",
+      width: "980px",
       backgroundColor: "white",
     },
     button: {
@@ -462,7 +458,7 @@ export default function Scheduler(props) {
       },
     },
 
-    duritionInput: {
+    durationInput: {
       backgroundColor: "#FAD399",
       width: "50%",
       height: "75px",
@@ -536,178 +532,214 @@ export default function Scheduler(props) {
   });
 
   const classes = useStyles();
-
+  //style={{ display: "flex", justifyContent: "center" }}
   return (
     <div>
       <div className={classes.container}>
-        <Row className={classes.rows}>
-          <Col style={{ display: "flex", justifyContent: "center" }}>
-            <img src={IMG} className={classes.img} alt="" />
-          </Col>
-          <Col>
-            <h1 className={classes.h1}>First time Client Package (online)</h1>
-            <p className={classes.content}>
-              Personalized Ayurvedic health plan, starter herbs and a 30 min
-              flup.
-            </p>
-            <p
-              className={classes.content}
-              style={{ fontFamily: "SFProDisplayMedium" }}
-            >
-              1 hr 30 min I $260
-            </p>
-            <p className={classes.content}>
-              {elementToBeRendered.title}
-              <br></br>
-              Duration: ({elementToBeRendered.duration})<br></br>
-              Price: {elementToBeRendered.cost}
-            </p>
-          </Col>
-        </Row>
-      </div>
-      <div
-        className={classes.CalendarContainer}
-        aria-label={"find a day to meet"}
-      >
-        <Row className={classes.calendarTimeTable}>
-          <Col className={classes.calendarBox}>
-            <h1
-              style={{
-                textAlign: "left",
-                color: "white",
-                fontFamily: "AvenirHeavy",
-              }}
-            >
-              Find a date to meet with us
-            </h1>
-            {/* <p className={classes.title}>Find a time to meet with Nitya </p> */}
-            <Calendar
-              backgroundColor="#d3a625"
-              calendarType="US"
-              onClickDay={dateChange}
-              value={date}
-              className={classes.center}
+        <Row style={{ padding: "0px", backgroundColor: "#B28D42" }}>
+          <Col style={{ padding: "0px" }}>
+            <img
+              src={elementToBeRendered.image_url}
+              className={classes.img}
+              alt=""
+              style={{ height: "100%" }}
             />
           </Col>
-          <Col className={classes.timeslotBox}>
-            <h1 className={classes.h1} style={{ textAlign: "left" }}>
-              What time is good for you?
-            </h1>
-            <p
-              style={{
-                textAlign: "left",
-                color: "#B28D42",
-                // fontFamily: "AvenirHeavy",
-                fontSize: "24px",
-              }}
-            >
-              UTC - 07:00 Pacific Time
-            </p>
-            <div className={classes.timeslotButtonBox}>
-              {renderAvailableApptsVertical()}
-            </div>
-          </Col>
-        </Row>
-      </div>
-
-      <div className={classes.container}>
-        <Row>
-          <Col>
-            <h1 className={classes.selectTime}>Confirm Meeting</h1>
-            <br></br>
+          <Col
+            style={{
+              padding: "30px 20px",
+            }}
+          >
             <h1
+              className={classes.h1}
+              style={{ color: "white", textAlign: "center" }}
+            >
+              {elementToBeRendered.title}
+            </h1>
+            <br />
+            <p className={classes.content} style={{ color: "white" }}>
+              {elementToBeRendered.description}
+            </p>
+            <br />
+            <p
+              className={classes.content}
               style={{
-                fontSize: "42x",
-                fontFamily: "AvenirHeavy",
+                fontFamily: "SFProDisplayMedium",
+                color: "white",
                 margin: "0 auto",
                 textAlign: "center",
               }}
-              hidden={timeSelected ? "hidden" : ""}
             >
-              Please pick a day and time to meet
-            </h1>
-            <h1 className={classes.date} hidden={!timeSelected ? "hidden" : ""}>
-              <span style={{}}>{dateString1}</span> at{" "}
-              <span style={{}}>{selectedTime}</span>
-            </h1>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <p className={classes.content}>
-              First time Client Package (online)
-              <br />1 hr 30 min I $260
+              {elementToBeRendered.duration} | {elementToBeRendered.cost}
             </p>
-            <br />
-            <p className={classes.content}>
-              {elementToBeRendered.title}
-              <br></br>
-              Duration: ({elementToBeRendered.duration})<br></br>
-              Price: {elementToBeRendered.cost}
-            </p>
-            <br />
-            <img src={IMG} className={classes.img} alt="" />
-            <br />
-            <p className={classes.content}>
-              6055 Meridian Ave #40, San Jose, CA 95120, USA
-              <br />
-              Office: 408 471 7004
-            </p>
-          </Col>
-          <Col>
-            <SimpleForm
-              field="First Name"
-              onHandleChange={handleFirstNameChange}
-            />
 
-            <br></br>
-            <SimpleForm field="Email Name" onHandleChange={handleEmailChange} />
-            <br></br>
-
-            <SimpleForm
-              field="Phone Number"
-              onHandleChange={handlePhoneNumChange}
-            />
-            <br></br>
-            <SimpleFormText field="Notes" onHandleChange={handleNotesChange} />
-            {props.notes}
-            <br></br>
-            <SimpleForm
-              field="Zip Code (Last Name)"
-              onHandleChange={handleLastNameChange}
-            />
-            <br></br>
-            <CardElement
-              elementRef={(c) => (this._element = c)}
-              // className={props.classes.element}
-              // options={options}
-              style={{
-                padding: "10px",
-                boxSizing: "border-box",
-                borderRadius: "20px",
-                fontColor: "#52330D",
-                fontSize: "20px",
-                margin: "5px auto",
-                border: "2px solid #52330D",
-                width: "100%",
-                fontFamily: "AvenirHeavy",
-                outline: "none",
-              }}
-            />
-
-            <div aria-label={"click button to book your appointment"}>
-              <div hidden={timeSelected ? "hidden" : ""}>
-                <button className={classes.buttonDisable}>Confirm</button>
-              </div>
-              <div hidden={!timeSelected ? "hidden" : ""}>
-                <button className={classes.button} onClick={bookAppt}>
-                  Pay Now
-                </button>
-              </div>
-            </div>
+            <button
+              className={classes.button}
+              hidden={bookNowClicked ? "hidden" : ""}
+              onClick={() => setBookNowClicked(!bookNowClicked)}
+            >
+              Book Now
+            </button>
           </Col>
         </Row>
       </div>
+      {bookNowClicked ? (
+        <div>
+          <div
+            className={classes.CalendarContainer}
+            aria-label={"find a day to meet"}
+          >
+            <Row className={classes.calendarTimeTable}>
+              <Col className={classes.calendarBox}>
+                <h1
+                  style={{
+                    textAlign: "left",
+                    color: "white",
+                    fontFamily: "AvenirHeavy",
+                  }}
+                >
+                  Find a date to meet with us
+                </h1>
+                {/* <p className={classes.title}>Find a time to meet with Nitya </p> */}
+                <Calendar
+                  backgroundColor="#d3a625"
+                  calendarType="US"
+                  onClickDay={dateChange}
+                  value={date}
+                  className={classes.center}
+                />
+              </Col>
+              <Col className={classes.timeslotBox}>
+                <h1 className={classes.h1} style={{ textAlign: "left" }}>
+                  What time is good for you?
+                </h1>
+                <p
+                  style={{
+                    textAlign: "left",
+                    color: "#B28D42",
+                    // fontFamily: "AvenirHeavy",
+                    fontSize: "24px",
+                  }}
+                >
+                  UTC - 07:00 Pacific Time
+                </p>
+                <div className={classes.timeslotButtonBox}>
+                  {renderAvailableApptsVertical()}
+                </div>
+              </Col>
+            </Row>
+          </div>
+          <div className={classes.container} style={{ padding: "40px 40px" }}>
+            <Row>
+              <Col>
+                <h1 className={classes.selectTime}>Confirm Meeting</h1>
+                <br></br>
+                <h1
+                  style={{
+                    fontSize: "42x",
+                    fontFamily: "AvenirHeavy",
+                    margin: "0 auto",
+                    textAlign: "center",
+                  }}
+                  hidden={timeSelected ? "hidden" : ""}
+                >
+                  Please pick a day and time to meet
+                </h1>
+                <h1
+                  className={classes.date}
+                  hidden={!timeSelected ? "hidden" : ""}
+                >
+                  <span style={{}}>{dateString1}</span> at{" "}
+                  <span style={{}}>{selectedTime}</span>
+                </h1>
+              </Col>
+            </Row>
+            <br />
+            <Row>
+              <Col>
+                <p className={classes.content} style={{ textAlign: "left" }}>
+                  {elementToBeRendered.title}
+                  <br />
+                  {elementToBeRendered.duration} | {elementToBeRendered.cost}
+                </p>
+                <br />
+                <img
+                  src={elementToBeRendered.image_url}
+                  className={classes.img}
+                  alt=""
+                />
+                <br />
+                <br />
+                <p className={classes.content} style={{ textAlign: "left" }}>
+                  6055 Meridian Ave #40, San Jose, CA 95120, USA
+                  <br />
+                  Office: 408 471 7004
+                </p>
+              </Col>
+              <Col>
+                <SimpleForm
+                  field="First Name"
+                  onHandleChange={handleFirstNameChange}
+                />
+
+                <br></br>
+                <SimpleForm
+                  field="Email Name"
+                  onHandleChange={handleEmailChange}
+                />
+                <br></br>
+
+                <SimpleForm
+                  field="Phone Number"
+                  onHandleChange={handlePhoneNumChange}
+                />
+                <br></br>
+                <SimpleFormText
+                  field="Notes"
+                  onHandleChange={handleNotesChange}
+                />
+                {props.notes}
+                <br></br>
+                <SimpleForm
+                  field="Zip Code (Last Name)"
+                  onHandleChange={handleLastNameChange}
+                />
+                <br></br>
+                <CardElement
+                  elementRef={(c) => (this._element = c)}
+                  // className={props.classes.element}
+                  // options={options}
+                  style={{
+                    padding: "10px",
+                    boxSizing: "border-box",
+                    borderRadius: "20px",
+                    fontColor: "#52330D",
+                    fontSize: "20px",
+                    margin: "5px auto",
+                    border: "2px solid #52330D",
+                    width: "100%",
+                    fontFamily: "AvenirHeavy",
+                    outline: "none",
+                  }}
+                />
+
+                <div aria-label={"click button to book your appointment"}>
+                  <div hidden={timeSelected ? "hidden" : ""}>
+                    <button className={classes.buttonDisable}>Confirm</button>
+                  </div>
+                  <div hidden={!timeSelected ? "hidden" : ""}>
+                    <button className={classes.button} onClick={bookAppt}>
+                      Pay Now
+                    </button>
+                  </div>
+                </div>
+              </Col>
+            </Row>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
