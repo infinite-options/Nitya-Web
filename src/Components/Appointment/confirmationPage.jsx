8bede0f2104c39fea6e35890eb765fed41b9b607
Brandon Huss <bmhuss33@gmail.com>
2021-07-16 14:48:37 -0700

    trying to get maps to work on confirmation page

--- src/Components/Appointment/confirmationPage.jsx
+++ src/Components/Appointment/confirmationPage.jsx
@@ -5,7 +5,11 @@ import { makeStyles } from "@material-ui/core/styles";
 import { useLocation } from 'react-router-dom';
 import "./calendar.css";
 import { ApptContext } from "./Scheduler";
-import herbsImg from '../../Mask Group 12.png';
+import herbsImg from '../../images/herbsImg.png';
+import MapSection from "../Home/Map";
+import { withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps";
+
+const google = window.google;
 
 const useStyles = makeStyles({
   container: {
@@ -80,8 +84,21 @@ const useStyles = makeStyles({
 });
 
 export default function ConfirmationPage(props) {
+  // const google = window.google;
+
+  console.log("window: ", window);
+  console.log("window.google: ", google);
+
   const classes = useStyles();
   const location = useLocation();
+
+  const scaleWidthFn = () => {
+    return 280-(810-dimensions.width)*0.4;
+  }
+
+  const scaleHeightFn = (y) => {
+    return 210-(810-dimensions.width)*0.3;
+  }
 
   console.log("(confirmationPage) props 1: ", props);
   console.log("(confirmationPage) props 2: ", props.location);
@@ -101,12 +118,43 @@ export default function ConfirmationPage(props) {
     }
   }); */
 
+  const [dimensions, setDimensions] = useState({ 
+    height: window.innerHeight,
+    width: window.innerWidth
+  });
+
+  useEffect(() => {
+    console.log("RERENDER -- window: ", window);
+    console.log("RERENDER -- window.google: ", window.google);
+    // console.log("RERENDER -- this.google: ", this.google);
+    function handleResize() {
+			setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth
+      });
+		}
+
+    window.addEventListener('resize', handleResize);
+
+		return _ => {
+      window.removeEventListener('resize', handleResize);
+		}
+  });
+
+  useEffect(() => {
+    // const map = new google.maps.Map(document.getElementById("map"), {
+    //   center: { lat: 37, lng: 110},
+    //   zoom: 12,
+    // });
+    // console.log();
+  }, []);
+
   return (
     <div
       style={{
         // border: "dashed",
         // height: "100vh",
-        height: "600px",
+        height: "860px",
         width: "100vw",
         maxWidth: "100%",
         backgroundColor: "#DADADA",
@@ -116,12 +164,48 @@ export default function ConfirmationPage(props) {
         // marginBottom: "500px"
       }}
     >
+      {/* For debugging window size */}
+			{/* <span 
+				style={{
+					zIndex: '101',
+					position: 'fixed',
+					backgroundColor: 'white',
+					border: 'solid',
+					borderWidth: '1px',
+					borderColor: 'red',
+					width: '150px',
+          left: '0'
+				}}
+			>
+				Height: {dimensions.height}px
+				<br />
+				Width: {dimensions.width}px
+			</span> */}
+      {/* width: `calc(${280-(810-dimensions.width)}px)` */}
+      {/* <span 
+				style={{
+					zIndex: '101',
+					position: 'fixed',
+					backgroundColor: 'white',
+					border: 'solid',
+					borderWidth: '1px',
+					borderColor: 'green',
+					width: '150px',
+          right: '0'
+				}}
+			>
+				Height: {scaleHeightFn(dimensions.width)}px
+				<br />
+				Width: {scaleWidthFn(dimensions.width)}px
+			</span> */}
       <div
         style={{
           backgroundColor: "white",
           width: "90%",
-          height: "540px",
-          maxWidth: "980px"
+          height: "800px",
+          maxWidth: "980px",
+          display: 'block',
+          overflow: 'auto'
         }}
       >
         <div
@@ -174,7 +258,7 @@ export default function ConfirmationPage(props) {
         </div>
         <div
           style={{
-            border: 'dashed',
+            // border: 'dashed',
             height: 'calc(100% - 174px)',
             display: 'inline-flex',
             width: '100%'
@@ -182,8 +266,8 @@ export default function ConfirmationPage(props) {
         >
           <div
             style={{
-              border: 'solid purple',
-              // borderRight: '1px solid #B28D42',
+              // border: 'solid purple',
+              borderRight: '1px solid #B28D42',
               height: '100%',
               // minWidth: 'calc(50%-30px)',
               // width: 'calc(50%-30px)',
@@ -195,9 +279,11 @@ export default function ConfirmationPage(props) {
             <span
               style={{
                 color: "#b28d42",
+                fontSize: '18px',
+                fontWeight: '500'
               }}
             >
-              First-time Customer Package (online)
+              First-Time Customer Package (Online)
             </span>
             <br />
             <span
@@ -232,21 +318,79 @@ export default function ConfirmationPage(props) {
               aria-label={"an image of " + post.blogTitle}
             /> */}
             <br />
+            {/* <div
+              style={{
+                position: 'relative',
+                // border: '1px solid green',
+                height: '224px'
+              }}
+            >
+              <img
+                // className={classes.img}
+                // src={`url(${herbsImg})`}
+                // src={herbsImg}
+                // src={require(herbsImg)}
+                src={require('../../Mask Group 12.png').default}
+                style={{
+                  position: 'absolute',
+                  width: "280px",
+                  maxWidth: '95%',
+                  // height: "210px",
+                  // height: '75vw',
+                  paddingBottom: '75%',
+                  margin: '20px 0 0 0'
+                }}
+              />
+            </div> */}
             <img
               // className={classes.img}
-              src={`url(${herbsImg})`}
-              style={{
-                width: "100px",
-                height: "100px"
+              // src={`url(${herbsImg})`}
+              // src={herbsImg}
+              // src={require(herbsImg)}
+              src={require('../../Mask Group 12.png').default}
+              style={dimensions.width > 810 ? {
+                width: '280px',
+                height: '210px',
+                // maxWidth: '95%',
+                // height: "210px",
+                // height: '75vw',
+                // paddingBottom: '75%',
+                margin: '20px 0 20px 0'
+              } : {
+                // width: `calc(${280-(810-dimensions.width)}px)`,
+                // height: `calc(${210+(810-dimensions.width)}px)`,
+                width: `${scaleWidthFn(dimensions.width)}px`,
+                height: `${scaleHeightFn(dimensions.width)}px`,
+                // height: `${dimensions.width-600}px`,
+                // maxWidth: '95%',
+                // height: "210px",
+                // height: '75vw',
+                // paddingBottom: '75%',
+                margin: '20px 0 20px 0'
               }}
             />
+            {/* <img
+              // className={classes.img}
+              // src={`url(${herbsImg})`}
+              // src={herbsImg}
+              // src={require(herbsImg)}
+              src={require('../../Mask Group 12.png').default}
+              style={{
+                width: "280px",
+                maxWidth: '95%',
+                // height: "210px",
+                // height: '75vw',
+                paddingBottom: '75%',
+                margin: '10px 0 10px 0'
+              }}
+            /> */}
             <br />
             <span
               style={{
                 color: "#b28d42",
               }}
             >
-              6055 Meridian Ave #40,
+              6055 Meridian Ave #40
             </span>
             <br />
             <span
@@ -263,13 +407,80 @@ export default function ConfirmationPage(props) {
                 color: "#b28d42",
               }}
             >
-              Office: 408 471 7004
+              Office: (408) 471-7004
             </span>
+            <br />
+            <div
+              id = "map"
+              style={{
+                height: '200px',
+                width: '200px',
+                border: '1px solid',
+                margin: '20px 0 20px 0',
+              }}
+            >
+              MAPS
+            </div>
+            {/* <div
+              style={{
+                position: 'relative',
+                border: '1px solid green',
+                height: '250px'
+              }}
+            > */}
+            {/* <MapSection location={{lat: 37, lng: 122}} zoomLevel={15}/> */}
+            {/* <div 
+              // className = {styles.googleMap} 
+              style={{
+                width: '280px',
+                height: '210px',
+                margin: '20px 0 0 0',
+                borderRadius: '25px',
+                border: '1px solid'
+                // backgroundColor: '#E8E8E8',
+                // display: 'flex',
+                // alignItems: 'center',
+                // justifyContent: 'center'
+              }}
+              // id = "map"
+            />    */}
+              <div
+                style={dimensions.width > 810 ? {
+                  width: '280px',
+                  height: '210px',
+                  margin: '20px 0 0 0',
+                  borderRadius: '25px',
+                  backgroundColor: '#E8E8E8',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center'
+                } : {
+                  // position: 'absolute',
+                  // width: "280px",
+                  // maxWidth: '95%',
+                  // height: "210px",
+                  // height: '75vw',
+                  // paddingBottom: '75%',
+                  width: `${scaleWidthFn(dimensions.width)}px`,
+                  height: `${scaleHeightFn(dimensions.width)}px`,
+                  // maxWidth: '95%',
+                  // height: '210px',
+                  margin: '20px 0 0 0',
+                  borderRadius: '25px',
+                  backgroundColor: '#E8E8E8',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center'
+                }}
+              >
+                Maps
+              </div>
+            {/* </div> */}
           </div>
           <div
             style={{
-              // borderLeft: '1px solid #B28D42',
-              border: 'solid cyan',
+              borderLeft: '1px solid #B28D42',
+              // border: 'solid cyan',
               height: '100%',
               // width: 'calc(50%-30px)',
               width: '50%',
@@ -277,50 +488,74 @@ export default function ConfirmationPage(props) {
               paddingLeft: '30px'
             }}
           >
-            <span>
-              If anything changes we will contact you at: 
-            </span>
-            <br />
-            <br />
-            <span>
-              {location.state.apptInfo.first_name}
-            </span>
-            <br />
-            <span>
-              {location.state.apptInfo.email}
-            </span>
-            <br />
-            <br />
-            <span
+            <div
               style={{
-                color: "#b28d42",
+                fontSize: '18px',
+                fontWeight: '500'
               }}
             >
-              How to prepare for your consulation:
-            </span>
-            <br />
-            <br />
+              If anything changes we will contact you: 
+            </div>
+            {/* <br />
+            <br /> */}
+            <div
+              style={{
+                fontSize: '22px',
+                fontWeight: '300',
+                margin: '16px 0 0 0'
+              }}
+            >
+              {location.state.apptInfo.first_name}
+            </div>
+            {/* <br /> */}
+            <div
+              style={{
+                fontSize: '22px',
+                fontWeight: '300',
+                // margin: '24px 0 0 0'
+              }}
+            >
+              {location.state.apptInfo.phone_no}
+            </div>
+            {/* <br />
+            <br /> */}
+            {/* <br /> */}
             <div
               style={{
                 color: "#b28d42",
+                fontSize: '20px',
+                margin: '40px 0 0 0'
+              }}
+            >
+              How to prepare for your consultation:
+            </div>
+            {/* <br /> */}
+            {/* <br /> */}
+            <div
+              style={{
+                color: "#b28d42",
+                fontSize: '14px',
+                // padding: '50px 50px 50px 50px',
+                margin: '12px 0 0 0'
                 // display: 'inline-block',
-                width: '50%',
-                border: 'dashed'
+                // width: '50%',
+                // border: 'dashed'
               }}
             >
               Bringing these things to the consultation will help us accelerate the process.
             </div>
-            <br />
-            <br />
+            {/* <br /> */}
+            {/* <br /> */}
             {/* <span>
               List of your current medication, diet, and food preferences
             </span> */}
             <div
               style={{
-                color: "#b28d42",
+                fontSize: '14px',
+                margin: '12px 0 0 0'
                 // display: 'inline-block',
-                width: '50%',
-                border: 'dashed'
+                // width: '50%',
+                // border: 'dashed'
               }}
             >
               List of your current medication, diet, and food preferences
@@ -330,52 +565,4 @@ export default function ConfirmationPage(props) {
       </div>
     </div>
   );
-  // return (
-  //   <div 
-  //     id="apptconfirm" 
-  //     style={{ 
-  //       backgroundColor: "#DADADA",
-  //       // border: "dashed"
-  //       height: "100%",
-  //       width: "100%"
-  //     }}
-  //   >
-  //     <ScrollToTop />
-  //     <div className={classes.container}>
-  //       <p>Booking Confirmed</p>
-  //       <p>We have sent a confirmation email to </p>
-
-  //       <Row>
-  //         <Col>
-  //           <p className={classes.content} style={{ textAlign: "left" }}>
-  //             <br />
-  //           </p>
-  //           <br />
-
-  //           <br />
-  //           <br />
-  //           <p className={classes.content} style={{ textAlign: "left" }}>
-  //             6055 Meridian Ave #40, San Jose, CA 95120, USA
-  //             <br />
-  //             Office: 408 471 7004
-  //           </p>
-  //         </Col>
-  //         <Col>
-  //           <p>If anything changes we will contact you at:</p>
-
-  //           <div>
-  //             <p>How to prepare for your consultation:</p>
-  //             <p>
-  //               Bringing these things to the consultation will help us
-  //               accelerate the process.
-  //             </p>
-  //             <p>
-  //               List of your current medication Your diet and food preferences
-  //             </p>
-  //           </div>
-  //         </Col>
-  //       </Row>
-  //     </div>
-  //   </div>
-  // );
 }
