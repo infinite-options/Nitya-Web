7cbbe8050655c2e9b77dbca63d4a93c18dc40a19
Marco <39marcom@gmail.com>
2021-07-20 18:16:19 -0700

    Added login/signup components + neccessary imports

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Cookies from "universal-cookie";
+import { AuthContext } from "./auth/AuthContext";
+
+// Component Imports
 import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer/Footer";
 import Homepage from "./Components/Homepage";
@@ -15,6 +19,8 @@ import FullBlog from "./Blog/FullBlog";
 import AddPost from "./Blog/AddPost";
 import ServicePage from "./Components/ServicePage";
 import AppointmentPage from "./Components/Appointment/AppointmentPage";
+import SignUp from "./Components/Home/SignUp";
+import Login from "./Components/Home/LogIn";
 
 //Stripe-related imports
 // import CheckoutForm from "./Components/Stripe/CheckoutForm";
@@ -30,14 +36,18 @@ import ConfirmationPage from "./Components/Appointment/confirmationPage";
 export const MyContext = React.createContext();
 
 function App() {
-  // const ELEMENTS_OPTIONS = {
-  //   fonts: [
-  //     {
-  //       cssSrc: "https://fonts.googleapis.com/css?family=Roboto",
-  //     },
-  //   ],
-  // };
-  // const stripePromise = loadStripe("pk_test_6pRNASCoBOKtIshFeQd4XMUh");
+  const cookies = new Cookies();
+  let uid =
+    cookies.get("customer_uid") == null ? "" : cookies.get("customer_uid");
+  let role = cookies.get("role") == null ? "" : cookies.get("role");
+  let guesProfile =
+    localStorage.getItem("guestProfile") == null
+      ? ""
+      : localStorage.getItem("guestProfile");
+  const [isGuest, setIsGuest] = useState(guesProfile === "" ? false : true); // checks if user is logged in
+  const [isAuth, setIsAuth] = useState(uid === "" ? false : true); // checks if user is logged in
+  const [authLevel, setAuthLevel] = useState();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const url =
     "https://mfrbehiqnb.execute-api.us-west-1.amazonaws.com/dev/api/v2/treatments";
@@ -53,47 +63,59 @@ function App() {
     }
   });
 
+  const login = () => {
+    setIsLoggedIn(true);
+  };
+
+  const logout = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
     <Router>
-      <Navbar />
+      <AuthContext.Provider
+        value={{
+          isLoggedIn: isLoggedIn,
+          login: login,
+          logout: logout,
+          isGuest,
+          setIsGuest,
+          isAuth,
+          setIsAuth,
+          authLevel,
+          setAuthLevel,
+        }}
+      >
+        <Navbar />
 
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route path="/home" component={Homepage} />
-        <Route path="/about" component={About} />
-        <Route path="/blog" component={Blog} />
-        <Route path="/:blog_uid/fullblog" component={FullBlog} />
-        <Route path="/addpost" component={AddPost} />
-        <Route path="/services" component={Services} />
-        <Route path="/contact" component={Contact} />
-        <Route exact path="/service" component={ServicePage} />
-        {/* <Route exact path="/appt" component={AppointmentPage} /> */}
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/home" component={Homepage} />
+          <Route path="/about" component={About} />
+          <Route path="/blog" component={Blog} />
+          <Route path="/:blog_uid/fullblog" component={FullBlog} />
+          <Route path="/addpost" component={AddPost} />
+          <Route path="/services" component={Services} />
+          <Route path="/contact" component={Contact} />
+          <Route exact path="/service" component={ServicePage} />
+          <Route exact path="/:treatmentID/appt/">
+            <MyContext.Provider value={{ serviceArr, servicesLoaded }}>
+              <AppointmentPage />
+            </MyContext.Provider>
+          </Route>
 
-        {/* <Route exact path="/:treatmentID/service/">
-          <MyContext.Provider value={{ serviceArr, servicesLoaded }}>
-            <ServicePage />
-          </MyContext.Provider>
-        </Route> */}
-        <Route exact path="/:treatmentID/appt/">
-          <MyContext.Provider value={{ serviceArr, servicesLoaded }}>
-            <AppointmentPage />
-          </MyContext.Provider>
-        </Route>
-        {/* <Route exact path="/:treatmentID/appt/stripe">
-          <MyContext.Provider value={{ serviceArr, servicesLoaded }}>
-            <Elements stripe={stripePromise} options={ELEMENTS_OPTIONS}>
-              <CheckoutForm />
-            </Elements>
-          </MyContext.Provider>
-        </Route> */}
-        <Route path="/apptconfirm">
-          <ConfirmationPage />
-        </Route>
-      </Switch>
-      {/*  <Route exact path="/" component={} />
-          <Route exact path="/blog" component={} /> */}
+          <Route path="/apptconfirm">
+            <ConfirmationPage />
+          </Route>
+          <Route path="/apptconfirm">
+            <ConfirmationPage />
+          </Route>
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/login" component={Login}></Route>
+        </Switch>
 
-      <Footer />
+        <Footer />
+      </AuthContext.Provider>
     </Router>
   );
 }
---
+++ src/Components/Home/SignUp.js
@@ -0,0 +1,68 @@
+import React from "react";
+
+import ScrollToTop from "../../Blog/ScrollToTop";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core";
+
+export default function SignUp(props) {
+  const useStyles = makeStyles({
+    signup: {
+      display: "flex",
+      justifyContent: "center",
+      color: "#594d2c",
+    },
+    heading: {
+      fontFamily: "DidoteTextW01-Italic",
+      fontSize: "4rem",
+    },
+    content: {
+      fontFamily: "'Open Sans', sans-serif",
+      fontSize: "1.5rem",
+    },
+    fbBtn: {
+      backgroundColor: "#3b5998",
+      width: "180px",
+    },
+    gBtn: {
+      backgroundColor: "#4285f4",
+    },
+    btn: {
+      border: "solid #594d2c 1px",
+      borderRadius: 0,
+      color: "#594d2c",
+      width: "300px",
+    },
+  });
+  const classes = useStyles();
+
+  return (
+    <div className={classes.signup} id="signup">
+      <ScrollToTop />
+
+      <div className={classes.container}>
+        <div>
+          <br></br>
+          <p className={classes.heading}>Sign Up</p>
+          <br></br>
+          <p className={classes.content}>
+            Already a member?
+            <a href="/login" aria-label={"click link to log in"}>
+              Log In
+            </a>
+          </p>
+        </div>
+        <br></br>
+        <hr></hr>
+        <div aria-label={"click button to sign up"} style={{ padding: 10 }}>
+          <Button className={classes.btn}>Sign up with email</Button>
+        </div>
+
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+      </div>
+    </div>
+  );
+}
