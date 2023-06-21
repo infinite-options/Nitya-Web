c61fb0a7ef1fc0466b287e326d049d11a55daa24
Jinseok Heo <jinseok.heo@sjsu.edu>
2023-06-21 14:47:31 -0700

    Added Dropdowns and Package tab on Services

---
+++ src/Services/ServiceDropdown.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import "../Home/Home.css";
+import "./ServiceDropdown.css"
+
+export default function ServiceDropdown(props) {
+    const {serviceArr, serviceType} = props.data;
+
+    return(
+        <div className="serivce-dropdown-content">
+            {serviceArr.length > 0 ? (
+            serviceArr
+                .filter((service) => service.category === serviceType)
+                .map((filteredService) => (
+                    <div key={filteredService.treatment_uid}>
+                        <Link
+                            to={{
+                                pathname: "/learnMore",
+                                state: {
+                                apptID: filteredService.treatment_uid,
+                                },
+                            }}
+                        >
+                            {filteredService.title}
+                        </Link>
+                    </div> 
+                ))
+            ) : (<>loading...</>)}
+        </div>
+    );
+}
---
+++ src/Services/ServiceList.jsx
@@ -0,0 +1,83 @@
+import LearnMoreBTN from "./LearnMoreBtn";
+import BookNowBTN from "../Services/BookNowBtn";
+import loadinggif from "../Assets/Images/loading.gif";
+import "../Home/Home.css";
+
+export default function ServiceList(props) {
+  const {serviceArr, serviceType} = props.data;
+  const data = serviceArr;
+  return (
+    <div className="ServiceCard">
+      <div
+        className="CardGrid"
+        style={{ gridTemplateColumns: "repeat(1, auto)", height: "auto" }}
+      >
+        {data.length > 0 ? (
+          data
+            .filter((service) => service.category === serviceType)
+            .map((filteredService) => (
+              <div
+                style={{
+                  display: "flex",
+                  backgroundColor: "#DADADA",
+                  marginTop: "2rem",
+                  maxHeight: "282px",
+                }} key={filteredService.treatment_uid}
+              >
+                <div
+                  style={{
+                    width: "100%",
+                    display: "flex",
+                    flex: "1",
+                    backgroundColor: "#DADADA",
+                    justifyContent: "center",
+                    maxHeight: "100%",
+                  }}
+                >
+                  <img
+                    style={{
+                      maxWidth: "100%",
+                      maxHeight: "100%",
+                      objectFit: " scale-down",
+                    }}
+                    className="ServiceImg"
+                    src={filteredService.image_url}
+                    alt={"An image of" + filteredService.title}
+                  />
+                </div>
+                <div style={{ flex: "1" }}>
+                  <div
+                    style={{
+                      width: "100%",
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "center",
+                      marginTop: "2rem",
+                      marginBottom: "2rem",
+                    }}
+                  >
+                    <div className="Services_Title_Font">
+                      {filteredService.title}
+                    </div>
+                    <div className="Services_Body_Font">
+                      {filteredService.description} <br />
+                    </div>
+                    <LearnMoreBTN
+                      apptID={filteredService.treatment_uid}
+                    />
+                    <BookNowBTN
+                      apptID={filteredService.treatment_uid}
+                    />
+                  </div>
+                </div>
+              </div>
+            ))
+        ) : (
+          <div>
+            <img src={loadinggif} alt="LOADING" className="loading-img"></img>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
